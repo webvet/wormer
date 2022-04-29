095f1c4c58fdf21f19650279c44a96edbde9328d
webvet <webvetservices@gmail.com>
2022-04-29 17:51:10 +0100

    testing props

--- src/App.js
+++ src/App.js
@@ -1,41 +1,16 @@
-import cat from './cat-icon.jpg';
-import dog from './dog-icon.jpg';
-/*import './App.css';
+import React, {useState} from 'react';
+import Tweet from './tweet';
 
 function App() {
+
+  const [isRed, setRed] = useState(false);
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="./cat-icon.jpg" className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
-*/
-import React from 'react';
-
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-      <img src={cat} alt="" width="50%" />
-      <img src={dog} alt="" width="50%" />
+      <div className="app">
+             
       </div>
-    )
-  }
+  );  
 }
 
 export default App;
---
+++ src/tweet.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function Tweet(props) {
+    return(
+        <div className="tweet">
+            <h3>{props.name}</h3>
+            <p>This is a random tweet</p>
+            <h3>Number of likes</h3>
+        </div>
+    );
+};
+
+export default Tweet;
